94a7826bbfed5cbfdcd4aa3b6e2f915ad27b0aaa
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-01 15:32:33 +0200

    memdel

--- libft.h
+++ libft.h
@@ -48,6 +48,7 @@ void	ft_putendl(char const *s);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
 
 // own funktions
 int		ft_ismatch(const char *haystack, const char *needle, int i);
