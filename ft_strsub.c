5c9b5d147cc336a3e01f0810f33474acf752d872
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-02 10:30:31 +0200

    strsub

---
+++ ft_strsub.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-maul <mde-maul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 10:16:02 by mde-maul          #+#    #+#             */
+/*   Updated: 2021/11/02 10:16:05 by mde-maul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	if ((start < 0 || start >= s_len) || (len < 0 || len > s_len - start))
+		return (NULL);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
