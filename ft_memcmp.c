0cdc4b488b8232aa71589ca615ba379c1b60cb50
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-03 10:59:52 +0200

    memcpm

---
+++ ft_memcmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-maul <mde-maul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 10:26:34 by mde-maul          #+#    #+#             */
+/*   Updated: 2021/11/03 10:26:38 by mde-maul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str1[i] && str2[i] && i < n)
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+		}
+		i++;
+	}
+	if ((i == n) || (str1[i] == '\0' && str2[i] == '\0'))
+		return (0);
+	return (str1[i] - str2[i]);
+}
--- libft.h
+++ libft.h
@@ -42,6 +42,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 // bonus
 void	ft_putchar(char c);
