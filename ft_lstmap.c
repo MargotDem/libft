8eeee0c05251e4b0c85d4e34672aa80ed9b97d77
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-04 14:13:11 +0200

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,20 +14,19 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*next;
 	t_list	*new_list_el;
 	t_list	*new_list;
+	t_list	*new_list_start;
 
-	new_list = ft_lstnew(lst->content, lst->content_size);
+	new_list = f(ft_lstnew(lst->content, lst->content_size));
+	new_list_start = new_list;
 	lst = lst->next;
 	while (lst)
 	{
-		next = lst->next;
 		new_list_el = f(ft_lstnew(lst->content, lst->content_size));
-		ft_lstadd_end(new_list, new_list_el);
-		//wow im really dumb. wtf am i doing. you can just:
-		//prev = new_list_el; before you reassign it
-		// prev->next = new_list_el;
-		lst = next;
+		new_list->next = new_list_el;
+		new_list = new_list_el;
+		lst = lst->next;
 	}
+	return (new_list_start);
 }
---
+++ tests/lstmap.c
@@ -0,0 +1,25 @@
+#include "../libft.h"
+
+t_list    *funktion(t_list *list_el)
+{
+	char	*content;
+	content = list_el->content;
+	content[3] = '!';
+	return list_el;
+}
+
+int main(void)
+{
+    t_list  *new_el1;
+    t_list  *new_el2;
+    t_list  *new_el3;
+    t_list  *new_list;
+
+    new_el1 = ft_lstnew("heyyyy", 20);
+    new_el2 = ft_lstnew("waaiitt", 30);
+    new_el3 = ft_lstnew("i got a new complainttt", 45);
+    ft_lstadd(&new_el3, new_el2);
+    ft_lstadd(&new_el2, new_el1);
+    new_list = ft_lstmap(new_el1, &funktion);
+	ft_display_list(new_list);
+}
