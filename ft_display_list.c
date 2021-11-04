479f38a1ea9f93433e4810db8914218df5145fde
margot <margot@ovrsea.com>
2021-11-04 10:29:05 +0200

    lstiter

--- ft_display_list.c
+++ ft_display_list.c
@@ -8,12 +8,5 @@ void    display_list_el(t_list *list_el)
 
 void    ft_display_list(t_list *list)
 {
-    t_list  *next;
-
-    while (list)
-    {
-        next = list->next;
-        display_list_el(list);
-        list = next;
-    }
+    ft_lstiter(list, &display_list_el);
 }
