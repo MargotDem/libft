452d18f5f1d92e745489372fdf1ac846b4279393
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-04 14:12:11 +0200

    norminette ft_display_list

--- ft_display_list.c
+++ ft_display_list.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_list.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-maul <mde-maul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 14:10:09 by mde-maul          #+#    #+#             */
+/*   Updated: 2021/11/04 14:10:12 by mde-maul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void    display_list_el(t_list *list_el)
+void	display_list_el(t_list *list_el)
 {
-    ft_putstr(list_el->content);
-    ft_putchar('\n');
+	ft_putstr(list_el->content);
+	ft_putchar('\n');
 }
 
-void    ft_display_list(t_list *list)
+void	ft_display_list(t_list *list)
 {
-    ft_lstiter(list, &display_list_el);
+	ft_lstiter(list, &display_list_el);
 }
