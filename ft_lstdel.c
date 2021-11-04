33d9d0534e380d9c239a90f203a05a234f4fec34
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-04 13:55:11 +0200

    fixed lstdel. maybe?

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -11,12 +11,10 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	//ok this makes NO SENSE AT ALL why not use the function we just coded smh??
-	//im gonn commit self murder soon lmao
-	//alright.
 	t_list	*list;
 	t_list	*next;
 
