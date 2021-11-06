548a946c80ca20a0d85ffe1e992d6cd8d1400653
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-06 18:09:31 +0200

    tests isdigit

--- tests/isdigit.c
+++ tests/isdigit.c
@@ -1,12 +1,23 @@
-//#include <ctype.h>
+#include <ctype.h>
 #include <stdio.h>
 #include "../libft.h"
+#include "tests.h"
 
 int	main(void)
 {
-	int	int1 = 125;
-	int	int2 = 9;
+	char	*c1 = "a";
+	print_test_descr("character", c1);
+	print_test_rslt_int(isdigit(*c1), ft_isdigit(*c1), isdigit(*c1) == ft_isdigit(*c1));
 
-	printf("is '%i' digit: %i\n", int1, ft_isdigit(int1));
-	printf("is '%i'digit: %i\n", int2, ft_isdigit(int2));
+	char	*c2 = "\\0";
+	print_test_descr("character", "\\0");
+	print_test_rslt_int(isdigit(*c2), ft_isdigit(*c2), isdigit(*c2) == ft_isdigit(*c2));
+
+	char	*c3 = "7";
+	print_test_descr("character", c3);
+	print_test_rslt_int(isdigit(*c3), ft_isdigit(*c3), isdigit(*c3) == ft_isdigit(*c3));
+
+	char	*c4 = "0";
+	print_test_descr("character", c4);
+	print_test_rslt_int(isdigit(*c4), ft_isdigit(*c4), isdigit(*c4) == ft_isdigit(*c4));
 }
