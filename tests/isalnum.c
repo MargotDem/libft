900040d38856c752052b1a33c34894b4b3ed48f9
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-06 18:16:02 +0200

    tests isalnum

--- tests/isalnum.c
+++ tests/isalnum.c
@@ -1,12 +1,23 @@
-//#include <ctype.h>
+#include <ctype.h>
 #include <stdio.h>
 #include "../libft.h"
+#include "tests.h"
 
 int	main(void)
 {
-	char	c1 = 'h';
-	char	c2 = '*';
+	char	*c1 = "a";
+	print_test_descr("character", c1);
+	print_test_rslt_int(isalnum(*c1), ft_isalnum(*c1), isalnum(*c1) == ft_isalnum(*c1));
 
-	printf("is '%c'alpha: %i\n", c1, ft_isalnum(c1));
-	printf("is '%c'alpha: %i\n", c2, ft_isalnum(c2));
+	char	*c2 = "\\0";
+	print_test_descr("character", "\\0");
+	print_test_rslt_int(isalnum(*c2), ft_isalnum(*c2), isalnum(*c2) == ft_isalnum(*c2));
+
+	char	*c3 = "7";
+	print_test_descr("character", c3);
+	print_test_rslt_int(isalnum(*c3), ft_isalnum(*c3), isalnum(*c3) == ft_isalnum(*c3));
+
+	char	*c4 = "0";
+	print_test_descr("character", c4);
+	print_test_rslt_int(isalnum(*c4), ft_isalnum(*c4), isalnum(*c4) == ft_isalnum(*c4));
 }
