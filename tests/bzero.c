5fcf4d364201c652ca4b851a6a00402a4308a371
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-07 17:41:48 +0200

    tests bzero

--- tests/bzero.c
+++ tests/bzero.c
@@ -1,13 +1,25 @@
-//#include <strings.h>
+#include <strings.h>
 #include "../libft.h"
 #include <stdio.h>
+#include "tests.h"
 
 int	main(void)
 {
-	char	str[10];
-	printf("the string then: %s\n", str);
-	ft_bzero((void *)str, 9);
-	//ft_memset((void *)str, '6', 9);
-	str[9] = '\0';
-	printf("the string now: %s\n", str);
+	char	str1_ft[4];
+	char	str1[4];
+	int		n1 = 3;
+	str1_ft[0] = 'a';
+	str1_ft[1] = 'b';
+	str1_ft[2] = 'c';
+	str1[0] = 'a';
+	str1[1] = 'b';
+	str1[2] = 'c';
+	str1_ft[3] = '\0';
+	str1[3] = '\0';
+	char	*desc1 = ft_strjoin("string ft: '", str1_ft);
+	print_test_descr(ft_strjoin(desc1, "', string lib: "), str1);
+	ft_bzero((void *)str1_ft, n1);
+	bzero((void *)str1, n1);
+
+	print_test_rslt_str(str1, str1_ft, ft_strcmp(str1_ft, str1) == 0);
 }
