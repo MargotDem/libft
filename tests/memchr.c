8ec1b67d31e4db5f5e8cac83152b1699bb437be2
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-08 11:31:14 +0200

    tests strcat

--- tests/memchr.c
+++ tests/memchr.c
@@ -8,40 +8,40 @@ int	main(void)
 	char	s1[6] = "1b345";
 	int		n1 = 6;
 	char	c1 = 'b';
-	char	*desc1 = ft_strjoin("string: ", s1);
-	print_test_descr(ft_strjoin(desc1, ", char: "), &c1);
+	char	*desc1 = ft_strjoin("string: '", s1);
+	print_test_descr(ft_strjoin(desc1, "', char: "), &c1);
 	print_test_descr_plus("n", ft_itoa(n1));
 	print_test_rslt_str(memchr(s1, c1, n1), ft_memchr(s1, c1, n1), memchr(s1, c1, n1) == ft_memchr(s1, c1, n1));
 
 	char	s2[5] = "ab8d";
 	int		n2 = 5;
 	char	c2 = '8';
-	char	*desc2 = ft_strjoin("string: ", s2);
-	print_test_descr(ft_strjoin(desc2, ", char: "), &c2);
+	char	*desc2 = ft_strjoin("string: '", s2);
+	print_test_descr(ft_strjoin(desc2, "', char: "), &c2);
 	print_test_descr_plus("n", ft_itoa(n2));
 	print_test_rslt_str(memchr(s2, c2, n2), ft_memchr(s2, c2, n2), memchr(s2, c2, n2) == ft_memchr(s2, c2, n2));
 
 	char	s3[4] = "abc";
 	int		n3 = 4;
 	char	c3 = '\0';
-	char	*desc3 = ft_strjoin("string: ", s3);
-	print_test_descr(ft_strjoin(desc3, ", char: "), "\\0");
+	char	*desc3 = ft_strjoin("string: '", s3);
+	print_test_descr(ft_strjoin(desc3, "', char: "), "\\0");
 	print_test_descr_plus("n", ft_itoa(n3));
 	print_test_rslt_str(memchr(s3, c3, n3), ft_memchr(s3, c3, n3), memchr(s3, c3, n3) == ft_memchr(s3, c3, n3));
 
 	char	s4[4] = "abc";
 	int		n4 = 4;
 	char	c4 = 'd';
-	char	*desc4 = ft_strjoin("string: ", s4);
-	print_test_descr(ft_strjoin(desc4, ", char: "), &c4);
+	char	*desc4 = ft_strjoin("string: '", s4);
+	print_test_descr(ft_strjoin(desc4, "', char: "), &c4);
 	print_test_descr_plus("n", ft_itoa(n4));
 	print_test_rslt_str(memchr(s4, c4, n4), ft_memchr(s4, c4, n4), memchr(s4, c4, n4) == ft_memchr(s4, c4, n4));
 
 	char	s5[7] = "abcdef";
 	int		n5 = 3;
 	char	c5 = 'd';
-	char	*desc5 = ft_strjoin("string: ", s5);
-	print_test_descr(ft_strjoin(desc5, ", char: "), &c5);
+	char	*desc5 = ft_strjoin("string: '", s5);
+	print_test_descr(ft_strjoin(desc5, "', char: "), &c5);
 	print_test_descr_plus("n", ft_itoa(n5));
 	print_test_rslt_str(memchr(s5, c5, n5), ft_memchr(s5, c5, n5), memchr(s5, c5, n5) == ft_memchr(s5, c5, n5));
 
--- tests/strcat.c
+++ tests/strcat.c
@@ -1,12 +1,18 @@
-//#include <string.h>
+#include <string.h>
 #include "../libft.h"
 #include <stdio.h>
+#include "tests.h"
 
 int	main(void)
 {
-	char	s1[10] = "abcde";
+	char	s1_ft[10] = "abcde";
+	char	s1[10];
 	char	s2[5] = "efgh";
 
-	ft_strcat(s1, s2);
-	printf("this is s1 now: %s\n", s1);
+	ft_strcpy(s1, s1_ft);
+	char	*desc1 = ft_strjoin("src string: '", s1);
+	print_test_descr(ft_strjoin(desc1, "', string to cat"), s2);
+	ft_strcat(s1_ft, s2);
+	strcat(s1, s2);
+	print_test_rslt_str(s1, s1_ft, ft_strcmp(s1, s1_ft) == 0);
 }
