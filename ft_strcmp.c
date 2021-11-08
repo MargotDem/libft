2aa74e4c08bd7ba4871ee0d23810a0f90c770d14
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-08 13:36:29 +0200

    tests strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -12,16 +12,18 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	int	i;
+	int				i;
+	unsigned char	c1;
+	unsigned char	c2;
 
 	i = 0;
 	while (s1[i] && s2[i])
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		c1 = (unsigned char)s1[i];
+		c2 = (unsigned char)s2[i];
+		if (c1 != c2)
+			return (c1 - c2);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- tests/strcmp.c
+++ tests/strcmp.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include "../libft.h"
-//#include <string.h>
+#include <string.h>
+#include "tests.h"
 
 int	main(void)
 {
-	char	string[20] = "test string1";
-	char	string2[20] = "test string";
+	char	s1[10] = "abcdeabcd";
+	char	s2[10] = "abcdeab6d";
+	char	*desc1 = ft_strjoin("string 1: '", s1);
+	print_test_descr(ft_strjoin(desc1, "', string 2: "), s2);
+	print_test_rslt_int(strcmp(s1, s2), ft_strcmp(s1, s2), strcmp(s1, s2) - ft_strcmp(s1, s2) == 0);
 
-	printf("result of strcmp: %i\n", ft_strcmp(string, string2));
+	char	s3[4] = "abc";
+	char	s4[4] = "abc";
+	char	*desc2 = ft_strjoin("string 1: '", s3);
+	print_test_descr(ft_strjoin(desc2, "', string 2: "), s4);
+	print_test_rslt_int(strcmp(s3, s4), ft_strcmp(s3, s4), strcmp(s3, s4) - ft_strcmp(s3, s4) == 0);
+
+	char	s5[4] = "abc";
+	char	s6[1] = "";
+	char	*desc3 = ft_strjoin("string 1: '", s5);
+	print_test_descr(ft_strjoin(desc3, "', string 2: "), s6);
+	print_test_rslt_int(strcmp(s5, s6), ft_strcmp(s5, s6), strcmp(s5, s6) - ft_strcmp(s5, s6) == 0);
+
+
+	char	s7[1] = "";
+	char	s8[1] = "";
+	char	*desc4 = ft_strjoin("string 1: '", s7);
+	print_test_descr(ft_strjoin(desc4, "', string 2: "), s8);
+	print_test_rslt_int(strcmp(s7, s8), ft_strcmp(s7, s8), strcmp(s7, s8) - ft_strcmp(s7, s8) == 0);
 }
