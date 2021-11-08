72e238355c70598739f1cea93ee34bce7e6c665f
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-08 16:31:30 +0200

    tests memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -21,15 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (str1[i] && str2[i] && i < n)
+	while (i < n)
 	{
 		if (str1[i] != str2[i])
-		{
 			return (str1[i] - str2[i]);
-		}
 		i++;
 	}
-	if ((i == n) || (str1[i] == '\0' && str2[i] == '\0'))
-		return (0);
-	return (str1[i] - str2[i]);
+	return (0);
 }
