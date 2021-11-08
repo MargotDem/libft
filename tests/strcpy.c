b2ed6ea3e2d3f314d412dd59567c15f50664ee96
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-08 11:12:13 +0200

    tests strcpy

--- tests/strcpy.c
+++ tests/strcpy.c
@@ -1,12 +1,34 @@
-//#include <string.h>
+#include <string.h>
 #include <stdio.h>
 #include "../libft.h"
+#include "tests.h"
 
 int	main(void)
 {
-	char src1[20] = "the string to copy";
-	char dst1[20] = "";
-	printf("len of src string: %i, and of dst string: %i\n", ft_strlen(src1), ft_strlen(dst1));
-	printf("this is the new string, supposedly: %s\n", ft_strcpy(dst1, src1));
-	printf("len of src string: %i, and of dst string: %i\n", ft_strlen(src1), ft_strlen(dst1));
+	// string 'abc'
+	char	src1[4];
+	char	dst1_ft[4];
+	char	dst1[4];
+	src1[0] = 'a';
+	src1[1] = 'b';
+	src1[2] = 'c';
+	src1[3] = '\0';
+	dst1[3] = '\0';
+	dst1_ft[3] = '\0';
+	print_test_descr("src string", src1);
+	ft_strcpy(dst1_ft, src1);
+	strcpy(dst1, src1);
+	print_test_rslt_str(dst1, dst1_ft, ft_strcmp(dst1, dst1_ft) == 0);
+
+	// empty string
+	char	src2[1];
+	char	dst2_ft[1];
+	char	dst2[1];
+	src2[0] = '\0';
+	dst2[0] = '\0';
+	dst2_ft[0] = '\0';
+	print_test_descr("src string", src2);
+	ft_strcpy(dst2_ft, src2);
+	strcpy(dst2, src2);
+	print_test_rslt_str(dst2, dst2_ft, ft_strcmp(dst2, dst2_ft) == 0);
 }
