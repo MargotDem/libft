30caef9e50efe4125ccb1dc74927f29cea49e7fc
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-09 20:33:09 +0200

    strnstr tests

--- tests/strnstr.c
+++ tests/strnstr.c
@@ -1,23 +1,87 @@
 #include <string.h>
 #include "../libft.h"
 #include <stdio.h>
+#include "tests.h"
 
 int	main(void)
 {
-	/*char	s1[10] = "abcdebkbl";
-	char	n1[4] = "bkb";
+	char	h1[50] = "this is the haystack with the needle in it";
+	char	n1[7] = "needle";
+	int	len1 = 50;
+	char	*desc1 = ft_strjoin("haystack: '", h1);
+	print_test_descr(ft_strjoin(desc1, "', needle: "), n1);
+	print_test_descr_plus("len is", ft_itoa(len1));
+	char	*r1_ft = ft_strnstr(h1, n1, len1);
+	char	*r1 = strnstr(h1, n1, len1);
+	print_test_rslt_str(r1, r1_ft, ft_strcmp(r1, r1_ft) == 0);
 
+	char	h2[50] = "haystack with a partial match: needull";
+	char	n2[7] = "needle";
+	int	len2 = 50;
+	char	*desc2 = ft_strjoin("haystack: '", h2);
+	print_test_descr(ft_strjoin(desc2, "', needle: "), n2);
+	print_test_descr_plus("len is", ft_itoa(len2));
+	char	*r2_ft = ft_strnstr(h2, n2, len2);
+	char	*r2 = strnstr(h2, n2, len2);
+	print_test_rslt_str(r2, r2_ft, !r2_ft);
 
-	printf("\n");
+	char	h3[50] = "haystack with a partial match: nee	";
+	char	n3[7] = "needle";
+	int	len3 = 50;
+	char	*desc3 = ft_strjoin("haystack: '", h3);
+	print_test_descr(ft_strjoin(desc3, "', needle: "), n3);
+	print_test_descr_plus("len is", ft_itoa(len3));
+	char	*r3_ft = ft_strnstr(h3, n3, len3);
+	char	*r3 = strnstr(h3, n3, len3);
+	print_test_rslt_str(r3, r3_ft, !r3_ft);
 
-	printf("s1 is: %s, n1 is: %s, and the result of ft_strstr(s1, n1): %s\n", s1, n1, ft_strstr(s1, n1));
-	printf("and this is the result with the real strstr: %s\n\n", strstr(s1, n1));
-	*/
+	char	h4[60] = "this is the haystack and the needle is empty string";
+	char	n4[1] = "";
+	int	len4 = 60;
+	char	*desc4 = ft_strjoin("haystack: '", h4);
+	print_test_descr(ft_strjoin(desc4, "', needle: "), n4);
+	print_test_descr_plus("len is", ft_itoa(len4));
+	char	*r4_ft = ft_strnstr(h4, n4, len4);
+	char	*r4 = strnstr(h4, n4, len4);
+	print_test_rslt_str(r4, r4_ft, ft_strcmp(r4, r4_ft) == 0);
 
-	const char *largestring = "abcdefg";
-	const char *smallstring = "bcd";
-	int	len = 3;
+	char	h5[1] = "";
+	char	n5[7] = "needle";
+	int	len5 = 10;
+	char	*desc5 = ft_strjoin("haystack: '", h5);
+	print_test_descr(ft_strjoin(desc5, "', needle: "), n5);
+	print_test_descr_plus("len is", ft_itoa(len5));
+	char	*r5_ft = ft_strnstr(h5, n5, len5);
+	char	*r5 = strnstr(h5, n5, len5);
+	print_test_rslt_str(r5, r5_ft, !r5_ft);
 
-	printf("result with my strstr: %s\n", ft_strnstr(largestring, smallstring, len));
-	printf("result with real strstr: %s\n", strnstr(largestring, smallstring, len));
+	char	h6[6] = "hello";
+	char	n6[13] = "hello needle";
+	int	len6 = 6;
+	char	*desc6 = ft_strjoin("haystack: '", h6);
+	print_test_descr(ft_strjoin(desc6, "', needle: "), n6);
+	print_test_descr_plus("len is", ft_itoa(len6));
+	char	*r6_ft = ft_strnstr(h6, n6, len6);
+	char	*r6 = strnstr(h6, n6, len6);
+	print_test_rslt_str(r6, r6_ft, !r6_ft);
+
+	char	h7[13] = "hello needle";
+	char	n7[7] = "needle";
+	int	len7 = 10;
+	char	*desc7 = ft_strjoin("haystack: '", h7);
+	print_test_descr(ft_strjoin(desc7, "', needle: "), n7);
+	print_test_descr_plus("len is", ft_itoa(len7));
+	char	*r7_ft = ft_strnstr(h7, n7, len7);
+	char	*r7 = strnstr(h7, n7, len7);
+	print_test_rslt_str(r7, r7_ft, !r7_ft);
+
+	char	h8[13] = "hello needle";
+	char	n8[7] = "needle";
+	int	len8 = 13;
+	char	*desc8 = ft_strjoin("haystack: '", h8);
+	print_test_descr(ft_strjoin(desc8, "', needle: "), n8);
+	print_test_descr_plus("len is", ft_itoa(len8));
+	char	*r8_ft = ft_strnstr(h8, n8, len8);
+	char	*r8 = strnstr(h8, n8, len8);
+	print_test_rslt_str(r8, r8_ft, ft_strcmp(r8, r8_ft) == 0);
 }
