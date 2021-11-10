130c5803d59f446ef4bfe0dcd64a2eeb7c009255
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 15:00:48 +0200

    strncpy tests

--- tests/strcpy.c
+++ tests/strcpy.c
@@ -6,27 +6,18 @@
 int	main(void)
 {
 	// string 'abc'
-	char	src1[4];
+	char	src1[4] = "abc";
 	char	dst1_ft[4];
 	char	dst1[4];
-	src1[0] = 'a';
-	src1[1] = 'b';
-	src1[2] = 'c';
-	src1[3] = '\0';
-	dst1[3] = '\0';
-	dst1_ft[3] = '\0';
 	print_test_descr("src string", src1);
 	ft_strcpy(dst1_ft, src1);
 	strcpy(dst1, src1);
 	print_test_rslt_str(dst1, dst1_ft, ft_strcmp(dst1, dst1_ft) == 0);
 
 	// empty string
-	char	src2[1];
+	char	src2[1] = "";
 	char	dst2_ft[1];
 	char	dst2[1];
-	src2[0] = '\0';
-	dst2[0] = '\0';
-	dst2_ft[0] = '\0';
 	print_test_descr("src string", src2);
 	ft_strcpy(dst2_ft, src2);
 	strcpy(dst2, src2);
--- tests/strncpy.c
+++ tests/strncpy.c
@@ -1,15 +1,37 @@
-//#include <string.h>
+#include <string.h>
 #include <stdio.h>
 #include "../libft.h"
+#include "tests.h"
 
 int	main(void)
 {
-	char src1[20] = "the str";
-	char dst1[20] = "";
-	int	n;
+	// string 'abc'
+	char	src1[4] = "abc";
+	char	dst1_ft[4];
+	char	dst1[4];
+	size_t	n1 = 4;
+	print_test_descr("src string", src1);
+	ft_strncpy(dst1_ft, src1, n1);
+	strncpy(dst1, src1, n1);
+	print_test_rslt_str(dst1, dst1_ft, ft_strcmp(dst1, dst1_ft) == 0);
 
-	n = 10;
-	printf("len of src string: %i, and of dst string: %i\n", ft_strlen(src1), ft_strlen(dst1));
-	printf("this is the new string, supposedly: %s\n", ft_strncpy(dst1, src1, n));
-	printf("len of src string: %i, and of dst string: %i\n", ft_strlen(src1), ft_strlen(dst1));
+	// string 'abc'
+	char	src2[4] = "abc";
+	char	dst2_ft[7];
+	char	dst2[7];
+	size_t	n2 = 7;
+	print_test_descr("src string", src2);
+	ft_strncpy(dst2_ft, src2, n2);
+	strncpy(dst2, src2, n2);
+	print_test_rslt_str(dst2, dst2_ft, ft_strcmp(dst2, dst2_ft) == 0);
+
+	// string 'abc'
+	char	src3[4] = "abc";
+	char	dst3_ft[4];
+	char	dst3[4];
+	size_t	n3 = 2;
+	print_test_descr("src string", src3);
+	ft_strncpy(dst3_ft, src3, n3);
+	strncpy(dst3, src3, n3);
+	print_test_rslt_str(dst3, dst3_ft, ft_strcmp(dst3, dst3_ft) == 0);
 }
