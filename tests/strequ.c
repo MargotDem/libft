6eab58a221549286827de463d594821841ec4653
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 17:04:46 +0200

    strequ

--- tests/strequ.c
+++ tests/strequ.c
@@ -6,7 +6,6 @@ int	main(void)
 {
 	char	str1[4] = "abc";
 	char	str2[4] = "abc";
-	ft_strjoin("string 1: '", str1);
 	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str1), "', string 2"), str2);
 	printf("\nresult of ft_strequ: %i\n", ft_strequ(str1, str2));
 
@@ -15,7 +14,6 @@ int	main(void)
 
 	char	str3[4] = "abc";
 	char	str4[4] = "";
-	ft_strjoin("string 1: '", str3);
 	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str3), "', string 2"), str4);
 	printf("\nresult of ft_strequ: %i\n", ft_strequ(str3, str4));
 }
--- tests/strjoin.c
+++ tests/strjoin.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "../libft.h"
+#include "tests.h"
 
 int	main(void)
 {
-	char	s1[20] = "abc";
-	char	s2[20] = "def";
+	char	s1[4] = "abc";
+	char	s2[4] = "def";
 
-	printf("result of ft_strjoin: %s\n", ft_strjoin(s1, s2));
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", s1), "', string 2"), s2);
+	printf("\nresult of ft_strjoin: %s\n", ft_strjoin(s1, s2));
+
+	printf("\n\n");
+
+	char	s3[4] = "abc";
+	char	s4[1] = "";
+
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", s3), "', string 2"), s4);
+	printf("\nresult of ft_strjoin: %s\n", ft_strjoin(s3, s4));
 }
