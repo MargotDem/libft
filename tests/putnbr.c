d1c6bac6408be57bd8acb303be1f324059b3ee0d
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 15:51:08 +0200

    putnbr

--- tests/putnbr.c
+++ tests/putnbr.c
@@ -1,41 +1,31 @@
 #include <stdio.h>
 #include "../libft.h"
+#include "tests.h"
 
 int	main(void)
 {
 	int	int1 = 567;
-	int	int2 = 0;
-	int	int3 = -258;
-	int	int4 = 2147483647;
-	int	int5 = -2147483648;
-
-	printf(" putnbr of %i", int1);
+	print_test_descr("number", ft_itoa(int1));
 	ft_putnbr(int1);
 	printf("\n");
-
-	printf(" putnbr of %i", int2);
+	
+	int	int2 = 0;
+	print_test_descr("number", ft_itoa(int2));
 	ft_putnbr(int2);
 	printf("\n");
 
-	printf(" putnbr of %i", int3);
+	int	int3 = -258;
+	print_test_descr("number", ft_itoa(int3));
 	ft_putnbr(int3);
 	printf("\n");
 
-	printf(" putnbr of %i", int4);
+	int	int4 = 2147483647;
+	print_test_descr("number", ft_itoa(int4));
 	ft_putnbr(int4);
 	printf("\n");
 
-	printf(" putnbr of %i", int5);
+	int	int5 = -2147483648;
+	print_test_descr("number", ft_itoa(int5));
 	ft_putnbr(int5);
 	printf("\n");
 }
-/*
-
-positive int
-0
-negative int
-min int
-max int
-
-
-*/
