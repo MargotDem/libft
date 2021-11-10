4911bb18ca8a1c272fd21d02eb6e8c41ebf0007b
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 17:00:03 +0200

    strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -17,17 +17,11 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-		{
 			return (0);
-		}
 		i++;
 	}
-	if (i == n)
-		return (1);
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (1);
-	return (0);
+	return (1);
 }
--- tests/strnequ.c
+++ tests/strnequ.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include "../libft.h"
-//#include <string.h>
+#include "tests.h"
 
 int	main(void)
 {
-	char	string[20] = "abc";
-	char	string2[20] = "abc2";
-	int		n = 3;
+	char	str1[4] = "abc";
+	char	str2[4] = "abc";
+	size_t	n1 = 4;
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str1), "', string 2"), str2);
+	printf("\nresult of ft_strnequ: %i\n", ft_strnequ(str1, str2, n1));
 
-	printf("result of ft_strequ: %i\n", ft_strnequ(string, string2, n));
+
+	printf("\n\n");
+
+	char	str3[4] = "abc";
+	char	str4[4] = "";
+	size_t	n2 = 3;
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str3), "', string 2"), str4);
+	printf("\nresult of ft_strnequ: %i\n", ft_strnequ(str3, str4, n2));
+
+	printf("\n\n");
+
+	char	str5[5] = "abcd";
+	char	str6[5] = "abce";
+	size_t	n3 = 3;
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str5), "', string 2"), str6);
+	printf("\nresult of ft_strnequ: %i\n", ft_strnequ(str5, str6, n3));
 }
