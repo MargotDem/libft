ba9a99f8c7e19de49a648369cd0c8ad9dfea194b
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 17:40:20 +0200

    strmap

--- tests/strmap.c
+++ tests/strmap.c
@@ -9,15 +9,8 @@ char	funktion(char c)
 
 int	main(void)
 {
-	char	str[4];
+	char	str[4] = "abc";
 	char	*returned_str;
-
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
-
-	//char	*ft_strmap(char const *s, char (*f)(char));
 	printf("this is the string: %s\n", str);
 	returned_str = ft_strmap(str, &funktion);
 	printf("this is the returned string after the string was passed through the funktion by strmap: %s\n", returned_str);
