344279f3c9681f993b624096bf31a6c3d4296df4
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 15:20:30 +0200

    memalloc

--- tests/memalloc.c
+++ tests/memalloc.c
@@ -1,28 +1,31 @@
 #include "../libft.h"
 #include <stdio.h>
+#include "tests.h"
 
 int	main(void)
 {
 	char	*str;
-	int		*nb;
-
 	int		i;
-
+	print_test_descr("test with a string", "");
+	printf("\n");
 	str = (char *)ft_memalloc(3);
 	i = 0;
 	while (i < 3)
 	{
-		printf("str[i] whatever it is: %c\n", str[i]);
+		printf("str[%i] now: %c\n", i, str[i]);
 		str[i] = 'A';
 		i++;
 	}
 	i = 0;
 	while (i < 3)
 	{
-		printf("and now str[i]: %c\n", str[i]);
+		printf("and now str[%i]: %c\n", i, str[i]);
 		i++;
 	}
 
+	printf("\n\n");
+	int		*nb;
+	print_test_descr("test with an int", "");
 	nb = (int *)ft_memalloc(4);
 	*nb = 56;
 	printf("\nthe int: %i\n", *nb);
