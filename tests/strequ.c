63ecc2ad0b255f6ae968d8198852f36c15aa29f1
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-10 16:51:56 +0200

    strequ tests

--- tests/strequ.c
+++ tests/strequ.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include "../libft.h"
-//#include <string.h>
+#include "tests.h"
 
 int	main(void)
 {
-	char	string[20] = "abc";
-	char	string2[20] = "abc";
+	char	str1[4] = "abc";
+	char	str2[4] = "abc";
+	ft_strjoin("string 1: '", str1);
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str1), "', string 2"), str2);
+	printf("\nresult of ft_strequ: %i\n", ft_strequ(str1, str2));
 
-	printf("result of ft_strequ: %i\n", ft_strequ(string, string2));
+
+	printf("\n\n");
+
+	char	str3[4] = "abc";
+	char	str4[4] = "";
+	ft_strjoin("string 1: '", str3);
+	print_test_descr(ft_strjoin(ft_strjoin("string 1: '", str3), "', string 2"), str4);
+	printf("\nresult of ft_strequ: %i\n", ft_strequ(str3, str4));
 }
