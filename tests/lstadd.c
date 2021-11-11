07933fca76defca901948e206f5c0d3240e89467
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-11 18:02:49 +0200

    lst add and llstnew

--- tests/lstadd.c
+++ tests/lstadd.c
@@ -10,6 +10,6 @@ int main(void)
     new_el2 = ft_lstnew("the angel from my nightmare", 30);
     new_el3 = ft_lstnew("the shadow in the background of the morgue", 45);
     ft_lstadd(&new_el3, new_el2);
-    ft_lstadd(&new_el2, new_el1);
+    ft_lstadd(&new_el2, NULL);
     ft_display_list(new_el1);
 }
