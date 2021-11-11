fbd7e7442a022bfe960b66d3323a9dd5e3c51013
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-11 19:01:47 +0200

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,18 +12,33 @@
 
 #include "libft.h"
 
+static void	del(void *ptr, size_t n)
+{
+	(void)n;
+	free(ptr);
+}
+
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new_list_el;
 	t_list	*new_list;
 	t_list	*new_list_start;
 
+	if (!f || !lst)
+		return ;
 	new_list = f(ft_lstnew(lst->content, lst->content_size));
+	if (!new_list)
+		return (NULL);
 	new_list_start = new_list;
 	lst = lst->next;
 	while (lst)
 	{
 		new_list_el = f(ft_lstnew(lst->content, lst->content_size));
+		if (!new_list_el)
+		{
+			ft_lstdel(&new_list_start, &del);
+			return (NULL);
+		}
 		new_list->next = new_list_el;
 		new_list = new_list_el;
 		lst = lst->next;
