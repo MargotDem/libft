2b0fe11a5e7d770b92f2673caee62de1a4af261b
margot <margot@ovrsea.com>
2021-11-25 10:48:18 +0200

    test strsub

--- tests/strsub.c
+++ tests/strsub.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "../libft.h"
-//#include <string.h>
+#include "tests.h"
 
 int	main(void)
 {
-	char	string[20] = "abc";
+	char	s1[4] = "abc";
 	unsigned int	start = 1;
 	size_t		len = 2;
-
-	printf("result of ft_strequ: %s\n", ft_strsub(string, start, len));
+	char	*d1 = ft_strjoin("the string: '", s1);
+	print_test_descr(ft_strjoin(d1, "', start: "), ft_itoa(start));
+	print_test_descr_plus("len: ", ft_itoa(len));
+	char	*expected = "bc";
+	char	* result = ft_strsub(s1, start, len);
+	print_test_rslt_str(result, expected, !ft_strcmp(result, expected));
 }
