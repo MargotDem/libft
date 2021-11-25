a54aaef2f5be16bc20c137f738e0df00a1a6993d
margot <margot@ovrsea.com>
2021-11-25 19:59:36 +0200

    test lstmap

--- tests/lstmap.c
+++ tests/lstmap.c
@@ -1,4 +1,11 @@
 #include "../libft.h"
+#include <stdio.h>
+
+void	del(void *ptr, size_t n)
+{
+	(void)n;
+	free(ptr);
+}
 
 t_list    *funktion(t_list *list_el)
 {
@@ -10,6 +17,7 @@ t_list    *funktion(t_list *list_el)
 
 int main(void)
 {
+    t_list  *list;
     t_list  *new_el1;
     t_list  *new_el2;
     t_list  *new_el3;
@@ -18,8 +26,13 @@ int main(void)
     new_el1 = ft_lstnew("heyyyy", 20);
     new_el2 = ft_lstnew("waaiitt", 30);
     new_el3 = ft_lstnew("i got a new complainttt", 45);
-    ft_lstadd(&new_el3, new_el2);
-    ft_lstadd(&new_el2, new_el1);
-    new_list = ft_lstmap(new_el1, &funktion);
+    list = new_el1;
+    ft_list_push_back(list, new_el2);
+    ft_list_push_back(list, new_el3);
+    printf("This is the original list:\n");
+    ft_display_list(list);
+    new_list = ft_lstmap(list, &funktion);
+    ft_lstdel(&list, &del);
+    printf("\nThis is the new list:\n");
 	ft_display_list(new_list);
 }
