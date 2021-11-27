7482c6805ec8ee7fbd45f004abcd76d04cdb79bf
margot <margot@ovrsea.com>
2021-11-27 13:55:52 +0200

    strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -14,14 +14,7 @@
 
 int	ft_strequ(char const *s1, char const *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i] || s2[i])
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
+	if (!s1 || !s2)
+		return (0);
+	return (ft_strcmp(s1, s2) == 0);
 }
