5be99734f9f89f0453d441812f871774bc92eea2
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-27 16:45:01 +0200

    norminette

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -14,9 +14,9 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t			i;
+	size_t				i;
 	const unsigned char	*src_str;
-	unsigned char	*dst_str;
+	unsigned char		*dst_str;
 
 	i = 0;
 	if (!dst && !src)
