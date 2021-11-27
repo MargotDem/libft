3151a3bad2ccd50aabfba5e053a28f58844dd02b
margot <margot@ovrsea.com>
2021-11-27 12:24:34 +0200

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,13 +15,13 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t			i;
-	unsigned char	*src_str;
+	const unsigned char	*src_str;
 	unsigned char	*dst_str;
 
 	i = 0;
 	if (!dst && !src)
-		return (dst);
-	src_str = (unsigned char *)src;
+		return (NULL);
+	src_str = (const unsigned char *)src;
 	dst_str = (unsigned char *)dst;
 	while (i < n)
 	{
