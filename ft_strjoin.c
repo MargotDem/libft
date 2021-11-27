e6a486a370adb76913bff2fc125071c8339d68bf
margot <margot@ovrsea.com>
2021-11-27 14:59:45 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -15,27 +15,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int		i;
-	int		len_s1;
-	int		len_s2;
 
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	str = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (!s1 || !s2)
+		return (NULL);
+	str = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
 	if (!str)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (s2[i])
-	{
-		str[len_s1 + i] = s2[i];
-		i++;
-	}
-	str[len_s1 + i] = '\0';
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
 	return (str);
 }
