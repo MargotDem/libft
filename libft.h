ee8ea5e6729e71ef48642a2cddc447d2c31792fd
margot <margot@ovrsea.com>
2021-11-27 14:02:38 +0200

    strncmp prototypegit status and strnequ

--- libft.h
+++ libft.h
@@ -35,7 +35,7 @@ int		ft_isalnum(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_strncmp(char *s1, char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
