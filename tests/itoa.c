d250a984ecdb7fbccb8a99bc536d1c18cf293c02
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2021-11-30 12:08:19 +0200

    removed tests from master

--- tests/itoa.c
+++
@@ -1,9 +0,0 @@
-#include "../libft.h"
-#include <stdio.h>
-
-int	main(void)
-{
-	//int i = -2147483648;
-	int i = 0;
-	printf("this is i: %i, and now the string: %s", i, ft_itoa(i));
-}
--- tests/putendl.c
+++
@@ -1,9 +0,0 @@
-#include "../libft.h"
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*s = "a string to write to stdout followed by a newline";
-	ft_putendl(s);
-	printf("abc");
-}
