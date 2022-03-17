e0c0d3f627eb8e4db3ce069c988854cfa821348a
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-03-17 17:39:33 +0200

    deleted print in new libft function

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static int	get_size(int nb, int base, int negative)
 {
